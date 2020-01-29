264858db05addf3c1adb35e5b77f87647f1f8f5e
Yuan-Hang Zhang <me@sailorzhang.com>
2020-01-29 20:43:20 +0800

    add eval

---
+++ eval.py
@@ -0,0 +1,31 @@
+import logging
+from pytorch_lightning import Trainer
+from argparse import ArgumentParser
+from models.model import AffWild2VA
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main(hparams):
+    # init module
+    model = AffWild2VA(hparams)
+
+    trainer = Trainer(
+        gpus=hparams.gpus,
+        nb_gpu_nodes=hparams.nodes,
+        distributed_backend='ddp' if hparams.distributed else 'dp'
+    )
+    trainer.test(model)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument('--gpus', type=str, default='2,3')
+    parser.add_argument('--nodes', type=int, default=1)
+
+    # give the module a chance to add own params
+    parser = AffWild2VA.add_model_specific_args(parser)
+    # parse params
+    hparams = parser.parse_args()
+
+    main(hparams)
