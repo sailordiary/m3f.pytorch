94fda17080b2385a27add3e7807198902192b11c
Yuan-Hang Zhang <me@sailorzhang.com>
2020-01-30 10:33:06 +0800

    submission file

---
+++ create_submission.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import time
+import logging
+
+import torch
+import numpy as np
+
+
+def run_ensemble(eval_list, score_list):
+    os.makedirs('ensembled_predictions', exist_ok=True)
+    video_names = open(eval_list, 'r').read().splitlines()
+    video_scores = {k: {'valence': None, 'arousal': None} for k in video_names}
+    score_names = open(score_list, 'r').read().splitlines()
+    nb_scores = len(score_names)
+    for i, fname in enumerate(score_names):
+        scores = torch.load(fname)
+        if i == 0:
+            for video_name in video_names:
+                video_scores[video_name]['valence'] = scores['valence_pred']
+                video_scores[video_name]['arousal'] = scores['arousal_pred']
+        else:
+            for video_name in video_names:
+                video_scores[video_name]['valence'] += scores['valence_pred']
+                video_scores[video_name]['arousal'] += scores['arousal_pred']
+    for video_name in enumerate(video_names):
+        with open(os.path.join('ensembled_predictions', video_name + '.txt')) as fp:
+            valence = video_scores[video_name]['valence'] / nb_scores
+            arousal = video_scores[video_name]['valence'] / nb_scores
+            fp.write('valence,arousal\n')
+            for v, a in zip(valence, arousal):
+                fp.write('{:.3f},{:.3f}\n'.format(v, a))
+            logging.debug('Done writing %d lines for video %s', len(valence), video_name)
+
+
+def parse_arguments():
+    """Parses command-line flags.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--list",
+        help="Text file containing names of videos to be evaluated on.",
+        type=argparse.FileType("r"),
+        required=True)
+    parser.add_argument(
+        "-s",
+        "--scores",
+        help="Text file containing paths of input prediction .pt files.",
+        type=argparse.FileType("r"),
+        required=True)
+    parser.add_argument(
+        "-v", "--verbose", help="Increase output verbosity.", action="store_true")
+    return parser.parse_args()
+  
+  
+def main():
+    start = time.time()
+    args = parse_arguments()
+    if args.verbose:
+      logging.basicConfig(level=logging.DEBUG)
+    del args.verbose
+    run_ensemble(**vars(args))
+    logging.info("Computed in %s seconds", time.time() - start)
+
+
+if __name__ == "__main__":
+    main()
