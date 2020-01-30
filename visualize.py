1d84425ecf288752c7b67450a269527e3dbd35b1
Yuan-Hang Zhang <me@sailorzhang.com>
2020-01-30 10:58:16 +0800

    add option

--- visualize.py
+++ visualize.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     data = torch.load(sys.argv[1])
     video_name = input('Video name: ')
     print ('{} frames'.format(len(data['valence_gt'][video_name])))
-    plot_results(data['valence_gt'][video_name],
-                smooth_predictions(data['valence_pred'][video_name]), 'valence')  
-    plot_results(data['arousal_gt'][video_name],
-                smooth_predictions(data['arousal_pred'][video_name]), 'arousal')
+    start = int(input('Start frame: '))
+    end = int(input('End frame: '))
+    plot_results(data['valence_gt'][video_name][start: end],
+                smooth_predictions(data['valence_pred'][video_name][start: end]), 'valence')  
+    plot_results(data['arousal_gt'][video_name][start: end],
+                smooth_predictions(data['arousal_pred'][video_name][start: end]), 'arousal')
