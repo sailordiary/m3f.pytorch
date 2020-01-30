4ce6531cb1a4a184d062df44a7a2c176c2763928
Yuan-Hang Zhang <me@sailorzhang.com>
2020-01-30 10:50:21 +0800

    cosmetic fixes

--- eval.py
+++ eval.py
@@ -20,7 +20,7 @@ def main(hparams):
 
 if __name__ == '__main__':
     parser = ArgumentParser(add_help=False)
-    parser.add_argument('--gpus', type=str, default='2,3')
+    parser.add_argument('--gpus', type=str, default='2')
     parser.add_argument('--nodes', type=int, default=1)
 
     parser.add_argument('--checkpoint', type=str, default='')
