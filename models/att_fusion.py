7ae8255349385cf07a5718316a9c5f28777a44f3
Yuan-Hang Zhang <me@sailorzhang.com>
2020-02-03 17:10:23 +0800

    typo

--- models/att_fusion.py
+++ models/att_fusion.py
@@ -6,7 +6,7 @@ from .rnn import GRU
 
 
 class AttFusion(nn.Module):
-    def __init__(self, hidden_dim=512, hidden_dim=128):
+    def __init__(self, input_dim=512, hidden_dim=128):
         super(AttFusion, self).__init__()
         self.scorer_a = GRU(input_dim, hidden_dim, 1, 1, 1)
         self.scorer_v = GRU(input_dim, hidden_dim, 1, 1, 1)
