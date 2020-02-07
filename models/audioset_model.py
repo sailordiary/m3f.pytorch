cb73433572d2ac0f967bc4247093d0104043df19
Yuan-Hang Zhang <me@sailorzhang.com>
2020-02-07 13:30:51 +0800

    change cyclic step_up

--- models/audioset_model.py
+++ models/audioset_model.py
@@ -117,7 +117,7 @@ class AudioSet(pl.LightningModule):
             return optimizer
         else:
             if self.hparams.scheduler == 'cyclic':
-                self.cyclic_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, self.hparams.min_lr, self.hparams.learning_rate, step_size_up=1200, cycle_momentum=self.hparams.optimizer == 'sgd')
+                self.cyclic_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, self.hparams.min_lr, self.hparams.learning_rate, step_size_up=480, cycle_momentum=self.hparams.optimizer == 'sgd')
                 return optimizer
             elif self.hparams.scheduler == 'exp':
                 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, self.hparams.decay_factor)
