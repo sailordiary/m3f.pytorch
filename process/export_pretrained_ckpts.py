7860a0931cff702cb839d783d5535127c2431d7a
Yuan-Hang Zhang <me@sailorzhang.com>
2020-02-07 15:43:31 +0800

    fix export

--- process/export_pretrained_ckpts.py
+++ process/export_pretrained_ckpts.py
@@ -13,9 +13,15 @@ if __name__ == '__main__':
         if k.startswith('visual.fc'): continue
         layer_id = int(k.split('.')[2])
         if layer_id < 12: # up to conv3
-            ckpt['state_dict'][k.replace('v2p', 'shared')] = w
+            new_key = k.replace('v2p', 'shared')
+            ckpt['state_dict'][new_key] = w
+            print ('Created {}'.format(new_key))
         else:
-            ckpt['state_dict']['visual.v_private.{}'.format(layer_id-12)] = w
-            ckpt['state_dict']['visual.a_private.{}'.format(layer_id-12)] = w
+            param_name = k.split('.')[-1]
+            new_key_v = 'visual.v_private.{}.{}'.format(layer_id-12, param_name)
+            new_key_a = 'visual.a_private.{}.{}'.format(layer_id-12, param_name)
+            ckpt['state_dict'][new_key_v] = w
+            ckpt['state_dict'][new_key_a] = w
+            print ('Created {}, {}'.format(new_key_v, new_key_a))
     torch.save(ckpt, 'video_checkpoint.pt')
     print ('Successfully saved pre-trained video checkpoint!')
