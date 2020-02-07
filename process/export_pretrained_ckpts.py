23d38cd8aaf7cfbc5b13ce20ccc4a44baa8e84c7
Yuan-Hang Zhang <me@sailorzhang.com>
2020-02-07 14:16:03 +0800

    add checkpoint merger

--- process/export_pretrained_ckpts.py
+++ process/export_pretrained_ckpts.py
@@ -1,5 +1,21 @@
+import sys
+from collections import OrderedDict
+
 import torch
 
 
 if __name__ == '__main__':
-    
+    # pretrained V2P (model.v2p)
+    video_ckpt = torch.load(sys.argv[1])
+    ckpt = OrderedDict()
+    ckpt['state_dict'] = OrderedDict()
+    for k, w in video_ckpt['state_dict'].items():
+        if k.startswith('visual.fc'): continue
+        layer_id = int(k.split('.')[2])
+        if layer_id < 12: # up to conv3
+            ckpt['state_dict'][k.replace('v2p', 'shared')] = w
+        else:
+            ckpt['state_dict']['visual.v_private.{}'.format(layer_id-12)] = w
+            ckpt['state_dict']['visual.a_private.{}'.format(layer_id-12)] = w
+    torch.save(ckpt, 'video_checkpoint.pt')
+    print ('Successfully saved pre-trained video checkpoint!')
--- process/merge_av_checkpoints.py
+++ process/merge_av_checkpoints.py
@@ -1,5 +1,23 @@
+import sys
+from collections import OrderedDict
+
 import torch
 
 
 if __name__ == '__main__':
-    
+    # pretrained GRU
+    audio_ckpt = torch.load(sys.argv[1])
+    # pretrained V2P (model.v2p)
+    video_ckpt = torch.load(sys.argv[2])
+    ckpt = OrderedDict()
+    ckpt['state_dict'] = OrderedDict()
+    for k, w in audio_ckpt['state_dict'].items():
+        # drop fc layers
+        if k.startswith('audio.fc'): continue
+        else: ckpt['state_dict'][k] = w
+    for k, w in video_ckpt['state_dict'].items():
+        if k.startswith('visual.gru_a.fc'): continue
+        elif k.startswith('visual.gru_v.fc'): continue
+        else: ckpt['state_dict'][k] = w
+    torch.save(ckpt, 'fused_av.pt')
+    print ('Saved merged A-V checkpoint!')
