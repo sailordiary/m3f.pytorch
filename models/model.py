93a730f8657f8108ba78d26c659357ba2d26bd6b
Yuan-Hang Zhang <me@sailorzhang.com>
2020-02-07 18:27:00 +0800

    remove audio dropout

--- models/model.py
+++ models/model.py
@@ -83,7 +83,7 @@ class AffWild2VA(pl.LightningModule):
                     nFCs=self.hparams.num_fc_layers
                 )
         if 'audio' in self.hparams.modality:
-            self.audio = GRU(200, self.hparams.num_hidden, 2, rnn_fc_classes, self.hparams.num_fc_layers, dropout=True)
+            self.audio = GRU(200, self.hparams.num_hidden, 2, rnn_fc_classes, self.hparams.num_fc_layers)# , dropout=True)
         if self.hparams.modality == 'audiovisual':
             if self.hparams.fusion_type == 'concat':
                 self.fusion = GRU(self.hparams.num_hidden * 2 + self.hparams.num_hidden * (2 if self.hparams.split_layer == 5 else 4),
